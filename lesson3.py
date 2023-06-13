f5935e37ae5cb0bfa2468e448a49c182c3ab59fd
Lun Hsu <test0320chatgpt@gmail.com>
2023-06-13 11:29:52 +0800

    user_click

--- lesson3.py
+++ lesson3.py
@@ -14,14 +14,18 @@ class Window(tk.Tk):
         print(s.theme_use())
         s.theme_use('clam')
         s.configure('Title.TLabel', foreground='blue',background='yellow',font=("Helvetica", "16"))
+        s.configure('Led.TButton', foreground='blue',background='yellow',font=("Arial", 16))
         title_label = ttk.Label(self, text="LED控制器",style='Title.TLabel')
         print(title_label.winfo_class())
         
         title_label.pack(pady=25, padx=100)
 
         led_btn = ttk.Button(
-            self, text="LED 開")
+            self, text="LED 開",style='Led.TButton',command=self.user_click)
         led_btn.pack(pady=(10,50))
+    
+    def user_click(self):
+        print("user click")
 
 
 if __name__ == "__main__":
