8d5c1855a5c98311ed411d84ebd6fa20a40e3c93
Lun Hsu <test0320chatgpt@gmail.com>
2023-06-13 11:04:52 +0800

    TLabel

--- lesson3.py
+++ lesson3.py
@@ -1,16 +1,28 @@
 import tkinter as tk
+from tkinter import ttk
+
+
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title('GUI')
+        self.title("視窗一")
         # self.geometry('380x400')
-        self.resizable(False,False)
-        title_label = tk.Label(self,text="LED控制器",font=('Helvetica','16'))
-        title_label.pack(pady=25,padx=100)
+        self.resizable(False, False)
+        s= ttk.Style()
+        print(s.theme_names())
+        print(s.theme_use())
+        s.theme_use('clam')
+        s.configure('TLabel', foreground='blue',background='yellow')
+        title_label = ttk.Label(self, text="LED控制器", font=("Helvetica", "16"))
+        print(title_label.winfo_class())
         
-        led_btn = tk.Button(self,text="LED 開",font=('Helvetica','16'),padx=20,pady=20)
-        led_btn.pack()
+        title_label.pack(pady=25, padx=100)
+
+        led_btn = ttk.Button(
+            self, text="LED 開")
+        led_btn.pack(pady=(10,50))
+
 
 if __name__ == "__main__":
     window = Window()
