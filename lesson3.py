3c50a57029bcec7e7ca6181a6a954fef7dffe690
Lun Hsu <test0320chatgpt@gmail.com>
2023-06-13 11:53:41 +0800

    borderwidth padding

--- lesson3.py
+++ lesson3.py
@@ -14,14 +14,15 @@ class Window(tk.Tk):
         print(s.theme_use())
         s.theme_use('clam')
         s.configure('Title.TLabel', foreground='blue',background='yellow',font=("Helvetica", "16"))
-        s.configure('Led.TButton', foreground='blue',background='yellow',font=("Arial", 16))
+        s.configure('Led.TButton', foreground='blue',background='yellow',font=("Arial", 16),borderwidth=0,padding=(10,20))
         title_label = ttk.Label(self, text="LED控制器",style='Title.TLabel')
         print(title_label.winfo_class())
         
         title_label.pack(pady=25, padx=100)
-
+        print(s.layout('TButton'))
         led_btn = ttk.Button(
             self, text="LED 開",style='Led.TButton',command=self.user_click)
+        
         led_btn.pack(pady=(10,50))
     
     def user_click(self):
