505e55fa108de2b44c738186e5e15d0b8b220e5a
Lun Hsu <test0320chatgpt@gmail.com>
2023-06-15 14:42:13 +0800

    建立datasource存放資料庫要用的資料

--- lesson3.py
+++ lesson3.py
@@ -1,11 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
 from gpiozero import LED
+import datasource
 
 class LEDButton(ttk.Button):
     def __init__(self,master,led,**kwargs):
         super().__init__(master,**kwargs)
         self.led = led
+        self.state= False
+        self.configure(command=self.user_click)
+        
+        s= ttk.Style()
+        s.theme_use('clam')        
+        s.configure('LEDClose.TButton', foreground='blue',background='yellow',font=("Arial", 16),borderwidth=5,padding=(10,20))
+        s.configure('LEDOpen.TButton', foreground='blue',background='yellow',font=("Arial", 16),borderwidth=5,padding=(10,20))
         
     def user_click(self):
         print(self.state)
@@ -35,15 +43,15 @@ class Window(tk.Tk):
         # print(s.theme_use())
         s.theme_use('clam')
         s.configure('Title.TLabel',font=("Helvetica", "16"))
-        s.configure('LEDClose.TButton', foreground='blue',background='yellow',font=("Arial", 16),borderwidth=5,padding=(10,20))
-        s.configure('LEDOpen.TButton', foreground='blue',background='yellow',font=("Arial", 16),borderwidth=5,padding=(10,20))
+        
         title_label = ttk.Label(self, text="LED控制器",style='Title.TLabel')
         print(title_label.winfo_class())
         
         title_label.pack(pady=25, padx=100)
         print(s.layout('TButton'))
-        self.led_btn = ttk.Button(
-        self,led=redLed,text="LED 開",style='LEDClose.TButton',command=self.user_click)
+        
+        self.led_btn = LEDButton(
+        self,led=redLed,text="LED 開",style='LEDClose.TButton')
         
         self.led_btn.pack(pady=(10,50))
         
@@ -52,6 +60,7 @@ class Window(tk.Tk):
 
 
 if __name__ == "__main__":
+    datasource.sayHello()
     led = LED(23)
     led.off()
     window = Window(led)
