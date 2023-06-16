5b97a3502184337b9604e2d5b4613ef73e955eca
Lun Hsu <test0320chatgpt@gmail.com>
2023-06-16 14:39:42 +0800

    呼叫class

--- lesson5.py
+++ lesson5.py
@@ -1,75 +1,7 @@
 #!/usr/bin/python3
+from rgbLed import RGBLed
 
-import RPi.GPIO as GPIO
-from time import sleep
-
-GPIO.setmode(GPIO.BCM)  # choose BCM numbering scheme.
-
-GPIO.setup(17, GPIO.OUT)  # set GPIO 17 as output for white led
-GPIO.setup(27, GPIO.OUT)  # set GPIO 27 as output for red led
-GPIO.setup(22, GPIO.OUT)  # set GPIO 22 as output for red led
-
-blue = GPIO.PWM(17, 100)
-green = GPIO.PWM(27, 100)
-red = GPIO.PWM(22, 255)
-i = 0
-
-
-# try:
-#     while True:
-#         if i < 100:
-#             i += 10
-#         else:
-#             i = 10
-#         blue.start(i)  # start blue led
-#         sleep(0.05)
-#         blue.stop()
-#         sleep(0.05)
-#         print(i)
-#         green.start(i)  # start green led
-#         sleep(0.05)
-#         green.stop()
-#         sleep(0.05)
-#         print(i)
-#         red.start(i)  # start red led
-#         sleep(0.05)
-#         red.stop()
-#         sleep(0.05)
-#         print(i)
-# except KeyboardInterrupt:
-#     blue.stop()
-#     red.stop()
-#     green.stop()
-#     GPIO.cleanup()  # clean up GPIO on CTRL+C exit
-#     print("END")
+if __name__ == "__main__":
+    rgb = RGBLed(22,27,17)
+    rgb.redlight()
     
-try:
-    while True:
-        # if i < 100:
-        #     i += 10
-        # else:
-        #     i = 10
-        # blue.start(i)  # start blue led
-        # red.start(i)
-        # sleep(0.05)
-        # blue.stop()
-        # red.stop()
-        # sleep(0.05)
-        # print(i)
-        
-        red.start(100)
-        green.start(1)
-        blue.start(1)  # start blue led        
-        sleep(1)
-        red.stop()
-        green.stop()
-        blue.stop()        
-        sleep(1)
-        print(i)
-        
-except KeyboardInterrupt:
-    blue.stop()
-    red.stop()
-    green.stop()
-    GPIO.cleanup()  # clean up GPIO on CTRL+C exit
-    print("END")
--- rgbLed.py
+++ rgbLed.py
@@ -10,6 +10,14 @@ class RGBLed():
         self.red = GPIO.PWM(red_pin,75)
         self.green = GPIO.PWM(green_pin,75)
         self.blue = GPIO.PWM(blue_pin,75)
+    
+    def redlight(self):
+        try:
+            while(True):
+                self.red.start(75)
+        except KeyboardInterrupt:
+            self.red.stop()
+            
         
     def close(self):
         self.red.stop()
