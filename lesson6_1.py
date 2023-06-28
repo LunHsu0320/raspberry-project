7836c171da1bbb65c0ca9c58c5097da6b86e7620
Lun Hsu <test0320chatgpt@gmail.com>
2023-06-28 15:05:05 +0800

    資寮傳輸成功

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,34 +1,34 @@
-import gpiozero as zero 
+import gpiozero as zero
 import RPi.GPIO as GPIO
 from time import sleep
 from datetime import datetime
 import requests
+
+
 
 if __name__ == "__main__":
     mcp3008_ch7 = zero.MCP3008(channel=7)
     mcp3008_ch6 = zero.MCP3008(channel=6)
     try:
         while True:
-            value =round(mcp3008_ch7.value*100)
-            print("光敏電阻值: ",value)
-            if value>20:
+            value = round(mcp3008_ch7.value*100)
+            print("光敏電阻値: ", value)
+            if value > 20:
                 print("光線亮")
             else:
-                print("光線暗")    
-                
+                print("光線暗") 
+
             datetimeStr = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                            
-            print("LM35",mcp3008_ch6.value*100*3.3)    
-            
-            response = requests.get("https://lunhsu0320-urban-space-guide-rj4w79rpjgjf479-8000.preview.app.github.dev/raspberry?time={datetimeStr}&light={value}&temprature={mcp3008_ch6.value}")
+
+            response = requests.get(f'https://lunhsu0320-bug-free-waddle-rj4w79rpjwp3x7vv-8000.preview.app.github.dev/raspberry?time={datetimeStr}&light={value}&temperature={mcp3008_ch6.value}')
             
             if response.ok:
-                print("update")
+                print("上傳資料成功")
                 print(response.text)
             else:
                 print(response.status_code)
-            
+
             sleep(5)
     except KeyboardInterrupt:
         GPIO.cleanup()
-        print("程式停止")            
+        print("程序退函数")
