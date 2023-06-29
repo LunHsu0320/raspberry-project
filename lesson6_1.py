3a8877754d6346cf8bd0996f5843e628bc174ea1
Lun Hsu <test0320chatgpt@gmail.com>
2023-06-29 15:17:57 +0800

    up06291518

--- lesson6_1.py
+++ lesson6_1.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
             datetimeStr = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-            response = requests.get(f'https://lunhsu0320-ubiquitous-doodle-jxj9r64757rf5jjr-8000.preview.app.github.dev/raspberry?time={datetimeStr}&light={value}&temperature={mcp3008_ch6.value}')
+            response = requests.get(f'https://fastapi-lun.onrender.com/raspberry?time={datetimeStr}&light={value}&temperature={mcp3008_ch7.value*1000}')
             
             if response.ok:
                 print("上傳資料成功")
